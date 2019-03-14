2f5e9aff00519a40772ff08bc2344613a10001df
Mr.PY <pranayprasad84@gmail.com>
2019-03-14 23:56:35 +0530

    Few updates to already working program making it better
    Logic for non alpha characters is done -- only alphabets would run the prgm otherwise error wud be printed
    
    redefined user_input function as starting_point (should be the last time)
    
    added new vars name1, name2 to not mess up the pattern of inputted names (ex: Pranay Prasad)
    
    In findign_flame() the way to figure out the value is changed from if else logic to mod division logic
    previously, any value greater than 5 wud be subtracted with 5 but if the resulting value is greater than 5 then, exception occurs as string is just 5 characters.
    
    TODO: May be GUI using tkinter.
    
    Future: way to insert a list of names and every name should be evaluated with names in other list.

--- Flame_Finder.py
+++ Flame_Finder.py
@@ -1,46 +1,46 @@
-def user_input():
-    person1 = input("Enter the First Person's name: ").replace(' ', '')
-    person2 = input("Enter the second Person's name: ").replace(' ', '')
-    word_count = count_letters(person1.lower(), person2.lower())
-    # print(word_count)
-    # print(finding_flame(word_count))
-    # print(person1, person2)
-    flame_character = finding_flame(word_count)
-    flame_definer(person1, person2, flame_character)
+def starting_point():
+    person1 = input("Enter the First Person's name: ")
+    name1 = person1.replace(' ', '')
+    person2 = input("Enter the second Person's name: ")
+    name2 = person2.replace(' ', '')
+    word_count = count_letters(name1.lower(), name2.lower())
+    # ---TODO: non-character type should print out a message---
+    if name1.isalpha() and name2.isalpha():
+        flame_character = finding_flame(word_count)
+        flame_definer(person1, person2, flame_character)
+    else:
+        print("Please enter valid names")
 
 
-# Extracting Words from names ###
-def count_letters(person1, person2):
-    common_characters = list(set(person1).intersection(set(person2)))
+# ---Extracting Unique letters as a word from names---
+def count_letters(name1, name2):
+    common_characters = list(set(name1).intersection(set(name2)))
     for character in common_characters:
-        person1 = person1.replace(character, '')
-        person2 = person2.replace(character, '')
-    word_length = len(person1 + person2)
+        name1 = name1.replace(character, '')
+        name2 = name2.replace(character, '')
+    word_length = len(name1 + name2)
     return word_length
 
 
-# TODO: flames logic and printing the flame character
-# Flames Logic
+# ---Flames Logic---
 def finding_flame(word_length):
     flame_word = 'FLAME'
-    if word_length > 5:
-        flame_value = word_length - 5
-    else:
-        flame_value = word_length
-    return(flame_word[flame_value - 1])
+    flame_value = word_length % 5
+    return flame_word[flame_value - 1]
 
 
-def flame_definer(person1, person2, flame_character):
+# ---flame output/final output---
+def flame_definer(name1, name2, flame_character):
     if flame_character == 'F':
-        print(f"{person1} and {person2} are destined to be Friends")
+        print(f"{name1} and {name2} are destined to be Friends")
     elif flame_character == 'L':
-        print(f"{person1} and {person2} are destined to be Lovers")
+        print(f"{name1} and {name2} are destined to be Lovers")
     elif flame_character == 'A':
-        print(f"{person1} and {person2} are destined to be Affectionate")
+        print(f"{name1} and {name2} are destined to be Affectionate")
     elif flame_character == 'M':
-        print(f"{person1} and {person2} are destined to be Married")
+        print(f"{name1} and {name2} are destined to be Married")
     elif flame_character == 'E':
-        print(f"{person1} and {person2} are destined to be Enemies")
+        print(f"{name1} and {name2} are destined to be Enemies")
 
 
-user_input()
+starting_point()
